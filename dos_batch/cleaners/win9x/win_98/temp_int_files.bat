59afe18dcf094a2e657a924476f8af99dbb03ba0
Brandon <grave_123@riseup.net>
2014-11-02 15:41:21 -0500

    Create temp_int_files.bat

---
+++ dos_batch/cleaners/win9x/win_98/temp_int_files.bat
@@ -0,0 +1,27 @@
+rem     <GISAH>
+rem    Copyright (C) <2012>  <Brandon Smith>
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or any later version.
+
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <http://www.gnu.org/licenses/>."
+
+@echo
+
+echo >> ..\GISAH\logs\temp_int_files_log.txt
+
+echo Now cleaning the Temporary Internet Files directory...
+
+del C:\WINDOWS\TEMPOR~1\
+
+echo Complete.
+
+pause
+
+cls
