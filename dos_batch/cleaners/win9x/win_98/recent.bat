146098e82eee29feb3e6ed3bf2db8215a32bc5a7
Brandon <grave_123@riseup.net>
2014-11-02 15:40:06 -0500

    Create recent.bat

---
+++ dos_batch/cleaners/win9x/win_98/recent.bat
@@ -0,0 +1,29 @@
+rem     <GISAH>
+rem    Copyright (C) <2012>  <Brandon Smith>
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or any later version.
+
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <http://www.gnu.org/licenses/>."
+
+@echo off
+
+echo >> ..\GISAH\logs\recent_log.txt
+
+
+echo Now cleaning the Recent directory
+
+
+del C:\WINDOWS\Recent
+
+echo Done!
+
+pause
+
+cls
