8af9c35babebcd1ed5bd9fd59e50a6e25cc3cc35
Brandon <grave_123@riseup.net>
2014-11-02 15:00:45 -0500

    Create about.bat

---
+++ dos_batch/misc/about.bat
@@ -0,0 +1,48 @@
+rem     <GISAH>
+rem    Copyright (C) <2012>  <Brandon Smith>
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or any later version.
+
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <http://www.gnu.org/licenses/>."
+
+
+@echo off
+
+title GISAH : About
+
+cls
+
+rem =======================================================
+
+echo *******************************************************
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo   GISAH 0.2.1
+echo   "GISAH stands for "Gisah is simple as hell", a multi-purpose tool for Windows and MS-DOS.
+echo   Copyright (C) 2012  Brandon Smith
+echo   This program is free software: you can redistribute it and/or modify
+echo   it under the terms of the GNU General Public License as published by
+echo   the Free Software Foundation, either version 3 of the License, or any later version."
+
+echo   "This program is distributed in the hope that it will be useful,
+echo   but WITHOUT ANY WARRANTY; without even the implied warranty of
+echo   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+echo   GNU General Public License for more details."
+
+echo   "You should have received a copy of the GNU General Public License
+echo   along with this program.  If not, see http://www.gnu.org/licenses/."
+
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ********************************************************
+
+pause
+
+cls
+
+call ..\GISAH\gisah.bat
